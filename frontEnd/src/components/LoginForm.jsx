b9f1346f3ab684e84eb9cfdd676d2b004bf5a990
Harikrishnan-Ashok <harikrishnanashok2001@gmail.com>
2024-03-21 23:30:36 +0530

    folders Updated

--- frontEnd/src/components/LoginForm.jsx
+++ frontEnd/src/components/LoginForm.jsx
@@ -36,6 +36,8 @@ const LoginForm = ({ toggle, setToggle, show, setShow, children, f1, f2,bcolor }
     toggle === 1 ? console.log("login") : console.log("sign up");
   }
 
+
+
   // Render the login form if show is true
   if (show) {
     return (
@@ -117,3 +119,31 @@ const LoginForm = ({ toggle, setToggle, show, setShow, children, f1, f2,bcolor }
 };
 
 export default LoginForm;
+
+
+  fetch("http://localhost:8000/createposts", {
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+          title: title,
+          content: content,
+          rating: rating
+      })
+  })
+  .then(res => {
+      if (!res.ok) {
+          throw new Error('Network response was not ok');
+      }
+      return res.json();
+  })
+  .then(data => {
+      console.log(data); // Log response from backend
+      // Optionally, you can reset the form after successful submission
+      document.getElementById("postForm").reset();
+  })
+  .catch(error => {
+      console.error('Error:', error); // Handle errors
+  });
+});
